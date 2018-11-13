5086af5a8bece275b70dc7971e083a6d2423ca21
anacondaxxl <staicut.mihail@yahoo.ro>
2018-11-13 19:22:28 +0200

    import

--- src/Ascii.java
+++ src/Ascii.java
@@ -2,7 +2,9 @@ public class Ascii {
     public static void main(String[] args) {
 
                 char number = 0;
-                while ( number <= 127 ) {
+                while (number <= 127 ) {
+                    System.out.print((int) number);
+                    System.out.print(' ');
                     System.out.println(number);
                     number++;
                 }
---
+++ src/For.java
@@ -0,0 +1,9 @@
+package src;
+
+public class For {
+    public static void main(String[] args) {
+        for (double i=10;i>=0;i-=0.5) {
+            System.out.println(i);
+        }
+    }
+}
