1a071c13c6c0df176ca33de2f1c5d7020602ca8a
anacondaxxl <staicut.mihail@yahoo.ro>
2018-11-13 20:02:27 +0200

    import

---
+++ src/Robot.java
@@ -0,0 +1,26 @@
+public class Robot  {
+    public void run() {
+        move();
+        move();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        move();
+        turnLeft();
+        move();
+        pickBeeper();
+        turnLeft();
+        turnLeft();
+        move();
+        move();
+        move();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        move();
+        putBeeper();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+}
