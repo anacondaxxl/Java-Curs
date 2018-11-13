a4ea4b4289c00e80066026dfffc47ef0385f9134
anacondaxxl <staicut.mihail@yahoo.ro>
2018-11-13 20:53:00 +0200

    import

--- src/Robot.java
+++ src/Robot.java
@@ -2,27 +2,15 @@ import stanford.karel.Karel;
 
 public class Robot extends Karel {
     public void run() {
-        move();
-        move();
-        turnLeft();
-        turnLeft();
-        turnLeft();
-        move();
-        turnLeft();
-        move();
-        pickBeeper();
-        turnLeft();
-        turnLeft();
-        move();
-        move();
-        move();
-        turnLeft();
-        turnLeft();
-        turnLeft();
-        move();
+turnLeft();
+while (frontIsClear()){
+    if (noBeepersPresent()) {
         putBeeper();
-        turnLeft();
-        turnLeft();
-        turnLeft();
+    }
+    move();
+}
+if(noBeepersPresent()) {
+    putBeeper();
+}
     }
 }
